2ec2458232c37f1e69fab27348cb13c3cd800a96
rohitasb <rohitas@antiersolutions.com>
2023-07-27 19:33:38 +0530

    refactored ladder solutions

--- computer_thinking/inverse_triangle.py
+++ computer_thinking/inverse_triangle.py
@@ -1,15 +1,21 @@
 from sys import argv
 
 def main():
-    inverseLadder(int(argv[1]))
+    input = int(argv[1])
+    inverseLadder(input)
+    inverseLadder2(input)
 
 
 def inverseLadder(side:int):
     for i in range(side,0,-1):
         print("* "*(i))
     
-def inverseLadder2(side:int):
-    for i in range(side,0,-1):
-        print("* "*(i))
+def inverseLadder2(N:int):
+    for i in range(N,0,-1):
+        for j in range(1,i+1,1):
+            print(j, end = ' ')
+        print()
+        
+        
         
 main()
--- computer_thinking/ladder.py
+++ computer_thinking/ladder.py
@@ -1,11 +1,35 @@
 from sys import argv
 
+
 def main():
-    ladder(int(argv[1]))
+    input = int(argv[1])
+    ladder(input)
 
 
 def ladder(side:int):
     for i in range(side):
         print("* "*(i+1))
         
+
+def ladder2(side:int):
+    for i in range(side):
+        num= i+1
+        print(f"{num} "*(i+1))
+
+
+def ladder3_0(side:int):
+    for i in range(side):
+        num=1
+        for j in range(i+1):
+            print(num, end=" ")
+            num+=1
+        print("")
+
+
+def ladder3_1(N: int):
+    for i in range(1, 1 + N):
+        for j in range(1, i + 1):
+            print(j, end = ' ')
+        print()
+
 main()
