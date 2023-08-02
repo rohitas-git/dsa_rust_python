4248585ba0fe98336ffb73da04feed76f4790a7e
Rohitas_b <rohitasbansal@icloud.com>
2023-08-02 20:58:34 +0530

    commiting small changes

--- computer_thinking/patterns/src/maths/gcd_lcd.rs
+++ computer_thinking/patterns/src/maths/gcd_lcd.rs
@@ -1,3 +1,6 @@
+
+// Optimized Euclidean Algorithm
+// Time: 
 pub fn gcd_optimal(mut a:u32, mut b:u32)->u32{
     while b != 0 {
         let remainder = a % b;
@@ -7,6 +10,33 @@ pub fn gcd_optimal(mut a:u32, mut b:u32)->u32{
     a
     
 }
+
+// Optimized Euclidean Algorithm (Recursive)
+// Time: 
+pub fn gcd_optimal_recursive(a:u32, b:u32)->u32{
+    if b == 0 {
+        return a;
+    }
+    
+    return gcd_optimal_recursive(b, a % b);
+    
+}
+
+// Euclidean Algorithm
+// Time: O(min(a,b))
+pub fn gcd_better(mut a: u32, mut b: u32) -> u32{
+    while a != b{
+
+        if a > b{
+            a = a - b;
+        }
+        else {
+            b = b -a ;
+        }
+    }
+    a
+}
+
 
 pub fn gcd_brute_force(a:u32, b:u32)->u32{
     let l = std::cmp::min(a,b);
